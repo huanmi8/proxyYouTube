6198be71cd5709927cf7561accf648384200f0be
huanmi8 <44668240+huanmi8@users.noreply.github.com>
2024-12-22 09:34:13 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Wallhaven Proxy',
-  description: 'A proxy for wallhaven.cc',
+  description: 'A proxy for Wallhaven',
 }
 
 export default function RootLayout({
@@ -12,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
